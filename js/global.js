d4eb69ad3a5d8197cf256ecf87b79a377550c77a
Abdul Ahad <csabdulahad@gmail.com>
2022-07-01 22:41:10 +0100

    js and css folder were dropped

--- js/global.js
+++
@@ -1,7 +0,0 @@
-/**
- * Any global project related JavaScript code, functions can be placed within
- * this file. Hati will automatically import this file where it finds the
- * @link Util::js() method in the HTML document. It will be the first js file
- * to be linked via Util::css() method.
- * */
-

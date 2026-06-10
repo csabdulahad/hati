6466271c75f28ec9240e320b9cf94bf52dc39a2a
Abdul Ahad <csabdulahad@gmail.com>
2026-06-11 00:12:22 +0100

    v7.0.50-beta

--- src/Util/Crypto.php
+++ src/Util/Crypto.php
@@ -15,11 +15,10 @@ abstract class Crypto
 	const ALGO_2Y = '2y';
 	const ALGO_ARGON2I = 'argon2i';
 	const ALGO_ARGONI2ID = 'argon2id';
-	const ALGO_MD5 = 'md5';
 
 	/**
-	 * Hash any string value with specified algorithm. It supports hashing with the following algorithms: md5,
-	 * bcypto, 2y, argon2i, argon2id. There are defined constant for each type with ALGO prefix in Crypto class.
+	 * Hash any string value with specified algorithm. It supports hashing with the following algorithms: bcypto, 2y,
+	 * argon2i, argon2id. There are defined constant for each type with ALGO prefix in Crypto class.
 	 *
 	 * @param string $value Any string value to be hashed
 	 * @param string $algo The algorithm to be used for hashing
@@ -28,15 +27,25 @@ abstract class Crypto
 	public static function hash(string $value, string $algo = self::ALGO_BCRYPT): string
 	{
 		$allowed = [
-			self::ALGO_BCRYPT, self::ALGO_2Y, self::ALGO_ARGON2I,
-			self::ALGO_ARGONI2ID, self::ALGO_MD5
+			self::ALGO_BCRYPT,  self::ALGO_2Y,
+			self::ALGO_ARGON2I, self::ALGO_ARGONI2ID
 		];
 
-		if (!in_array($algo, $allowed))
-			throw new InvalidArgumentException('Invalid argument. Allowed algorithms are: md5, bcypto, 2y, argon2i, argon2id');
+		if (!in_array($algo, $allowed)) {
+			throw new InvalidArgumentException('Invalid argument. Allowed algorithms are: bcypto, 2y, argon2i, argon2id');
+		}
 
-		if ($algo === 'md5') return md5($value);
 		return password_hash($value, $algo);
 	}
 
+	public static function sha256(string $value): string
+	{
+		return hash('sha256', $value);
+	}
+	
+	public static function md5(string $value): string
+	{
+		return md5($value);
+	}
+	
 }
